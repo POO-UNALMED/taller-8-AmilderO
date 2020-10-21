4713411a2169a9aa855b056f15dc3937b55fbab4
AMILDER07 <amilder55@gmail.com>
2020-10-20 19:46:35 -0500

    Test 6

--- src/main/java/futbol/Jugador.java
+++ src/main/java/futbol/Jugador.java
@@ -23,6 +23,6 @@ public class Jugador extends Futbolista {
         return super.getEdad() - o.getEdad();
     }
     public String toString(){
-        return "El futbolista "+getNombre()+" tiene "+getEdad()+ ", y juega de "+getPosicion()+" con el dorsal "+dorsal+ ". Ha marcado "+golesMarcados;
+        return "El futbolista "+this.getNombre()+" tiene "+this.getEdad()+ ", y juega de "+this.getPosicion()+" con el dorsal "+dorsal+ ". Ha marcado "+golesMarcados;
     }
 }
